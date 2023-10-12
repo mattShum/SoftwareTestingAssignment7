2529986660f891847968672e0b5a1a3d7d0f2b89
Matt Shuman <cerotulaki@gmail.com>
2023-10-11 21:47:12 -0400

    Adding initial commit of the code

---
+++ src/Function.java
@@ -0,0 +1,48 @@
+public class Function {
+	
+	public static void main(String[] args) {
+		
+		int x = 4;
+		int k = -18;
+		int y = 17;
+		
+		System.out.println("The result is: " + Fun(x, k, y));
+		
+	}
+	
+	public static int Fun(int x, int k, int y){
+		System.out.println("Edge A");
+		int i;
+		System.out.println("Edge B");
+		for (i = x; i < y; i++){
+			System.out.println("Branch 1 - True");
+			if (x%5 < 3){
+				System.out.println("Branch 2 - True");
+				k = y - x;
+				y = y + i;
+				System.out.println("Edge C");
+			}
+			else{
+				System.out.println("Branch 2 - False");
+				x = k - 2;
+				y = y + x;
+				System.out.println("Edge D");
+			}
+			y = y - x;
+			i = i + 2;
+			System.out.println("Edge E");
+		}
+		System.out.println("Branch 1 - False");
+		if (x < 0 || y < 0){
+			System.out.println("Branch 3 - True");
+			k = y - x;
+			System.out.println("Edge F");
+		}
+		else{
+			System.out.println("Branch 3 - False");
+			x = y + k;
+			System.out.println("Edge G");
+		}
+		return x;
+	}
+}
